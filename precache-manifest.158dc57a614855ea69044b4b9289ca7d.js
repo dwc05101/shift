51033eef3195de9ff26caa2488133f915ea7aa07
dwc05101 <dwc05101@kaist.ac.kr>
2019-08-17 04:52:45 +0900

    Updates

---
+++ precache-manifest.158dc57a614855ea69044b4b9289ca7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2f64242e7aab87942ed1b632d376de5",
+    "url": "/shift/index.html"
+  },
+  {
+    "revision": "3d351a751f28c3961900",
+    "url": "/shift/static/css/2.a317ec87.chunk.css"
+  },
+  {
+    "revision": "3d351a751f28c3961900",
+    "url": "/shift/static/js/2.9681833c.chunk.js"
+  },
+  {
+    "revision": "d35622f8c5007202a7a4",
+    "url": "/shift/static/js/main.d6d0d219.chunk.js"
+  },
+  {
+    "revision": "d0e4573d6dbbf4f29963",
+    "url": "/shift/static/js/runtime~main.42dbfaa6.js"
+  }
+]);
